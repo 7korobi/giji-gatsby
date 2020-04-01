dd449952b32a1c6b4531ad0d577c49369f749da5
7korobi <7korobi@gmail.com>
2020-04-02 01:58:51 +0900

    copy resources.

---
+++ src/config/jest.js
@@ -0,0 +1,22 @@
+const { is_windows } = require("./os");
+
+let testRegex = "/__tests__/.*spec.coffee$";
+if (is_windows) {
+  testRegex = "\\\\__tests__\\\\.*spec.coffee$";
+}
+
+module.exports = {
+  testRegex,
+  moduleFileExtensions: ["coffee", "js", "json", "vue", "pug", "yml"],
+  moduleNameMapper: {
+    "^~/(.*)$": "<rootDir>/$1",
+    "^@/(.*)$": "<rootDir>/$1"
+  },
+  moduleDirectories: ["node_modules", "."],
+  transform: {
+    "^.+\\.js$": "babel-7-jest",
+    "^.+\\.yml$": "yaml-jest",
+    "^.+\\.pug$": "pug-jest",
+    "^.+\\.vue$": "vue-jest"
+  }
+};
