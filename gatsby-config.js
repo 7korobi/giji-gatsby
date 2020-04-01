5f51dbc5bf3dc4d7f7265703f88e85bcbf66b4cc
7korobi <7korobi@gmail.com>
2020-04-02 02:24:19 +0900

    hello gatsby typescript

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,16 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `7korobi`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-typescript`,
+      options: {
+        isTSX: true,
+        allExtensions: true,
+      },
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
---
+++ src/pages/index.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useReducer } from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+export default IndexPage
+function IndexPage() {
+  const [list, setList] = useReducer((state, newVal) => newVal, [])
+  async function init(){
+    if ( new Date().getTime() < list?.timeout ?? 0 ) { return }
+    try {
+      const res = await fetch('https://giji-api.duckdns.org/api/plan/progress')
+      const { plans } = await res.json()
+      plans.timeout = new Date().getTime() + 10000
+      setList( plans )
+    } catch (e) {
+      setList([])
+    }
+  }
+  useEffect(()=>{
+    const p = init()
+    return () => {
+      console.warn( p, list )
+    }
+  })
+
+  return (<Layout>
+    <SEO title="Home" />
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+  )
+}
